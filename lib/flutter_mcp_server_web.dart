a9e5a910e56ad73df8b9a8a9dc106908ff3ac682
jsha21 <jsha@ais21.co.kr>
2025-03-27 12:58:20 +0900

    Update to v0.1.4

---
+++ lib/flutter_mcp_server_web.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+/// Web implementation of the flutter_mcp_server plugin.
+class FlutterMcpServerWeb {
+  static void registerWith(Registrar registrar) {
+  }
+}
